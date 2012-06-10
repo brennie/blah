202a61359c0bc7a4fb39d6639ea61ab6650846ae
Barret Rennie <barret.rennie@gmail.com>
2012-06-09 21:12:55 -0600

    Added pagination to index and tag views.
    pages_template.html is used for 'older' and 'newer' controls

--- blah/views/tag.py
+++ blah/views/tag.py
@@ -1,7 +1,21 @@
-from flask import g, render_template
+from flask import g, render_template, request, url_for
 from pymongo import DESCENDING
 
 def get(tag):
-    posts = g.db.posts.find({'tags': tag}).sort('datetime', DESCENDING).limit(10)
+    page = int(request.args.get("page", 1))
 
-    return render_template("tag.html", posts=posts)
+    posts = g.db.posts.find({'tags': tag}).sort('datetime', DESCENDING).skip((page - 1) * 10).limit(10)
+
+    newer = None
+    older = None
+    
+    if page > 1:
+        if page == 2:
+            newer = url_for("tag", tag=tag)
+        else:
+            newer = url_for("tag", tag=tag, page=page - 1)
+
+    if posts.count() > page * 10:
+        older = url_for("tag", tag=tag, page=page + 1)
+    
+    return render_template("tag.html", posts=posts, newer=newer, older=older)
